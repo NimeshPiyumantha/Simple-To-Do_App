e814a9110a5f9fabb54de07824118491dce4b8a7
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-26 17:59:04 +0530

    Update Content

---
+++ src/components/Task.tsx
@@ -0,0 +1,17 @@
+import { ITask } from "@/types/tasks";
+import React from "react";
+
+interface TaskProps {
+  task: ITask;
+}
+
+const Task: React.FC<TaskProps> = ({ task }) => {
+  return (
+    <tr key={task.id}>
+      <td>{task.text}</td>
+      <td>{}</td>
+    </tr>
+  );
+};
+
+export default Task;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import { ITask } from "@/types/tasks";
 import React from "react";
+import Task from "./Task";
 
 interface TodoListProps {
   tasks: ITask[];
@@ -18,10 +19,7 @@ const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
         </thead>
         <tbody>
           {tasks.map((task) => (
-            <tr key={task.id}>
-              <td>{task.text}</td>
-              <td>{}</td>
-            </tr>
+            <Task key={task.id} task={task}/>
           ))}
         </tbody>
       </table>
