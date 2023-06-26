da2dd52bf36ff85893f03885faf2ce55fa4a1131
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-26 16:23:28 +0530

    Add AddTask components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import AddTask from "@/components/AddTask";
+
 export default function Home() {
   return (
     <main className="max-w-4xl mx-auto mt-4">
       <div className="text-center my-5 flex flex-col gap-4">
         <h1 className="text-2xl font-bold text-red-500">Todo List App</h1>
+        <AddTask/>
       </div>
     </main>
   );
