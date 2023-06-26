07de7df7c9a40aeae8dd832a14349056c7162085
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-26 16:31:58 +0530

    Add AiOutlinePlus components

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import {AiOutlinePlus} from "react-icons/ai"
 
 const AddTask = () => {
   return (
     <div>
-      <button className="btn btn-primary w-full">Add New task</button>
+      <button className="btn btn-primary w-full">Add New task <AiOutlinePlus className="ml-2" size={18}/></button>
     </div>
   );
 };
