f0f02eac806e04d882ba67d7fc72d1dc210b545f
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-26 16:44:58 +0530

    Add Table components

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const TodoList = () => {
-  return <div></div>;
+  return (
+    <div className="overflow-x-auto">
+      <table className="table w-full">
+        {/* head */}
+        <thead>
+          <tr className="bg-gray-200">
+            <th>Name</th>
+            <th>Favorite Color</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* row 1 */}
+          <tr>
+            <td>Cy Ganderton</td>
+            <td>Blue</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default TodoList;
