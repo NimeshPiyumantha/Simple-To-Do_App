34dc60936911b0e11e189365131a2df926cf3475
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-26 17:38:43 +0530

    Add getAllTodos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { getAllTodos } from "@/api/api";
 import AddTask from "@/components/AddTask";
 import TodoList from "@/components/TodoList";
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await getAllTodos();
+  console.log(tasks);
+
   return (
     <main className="max-w-4xl mx-auto mt-4">
       <div className="text-center my-5 flex flex-col gap-4">
