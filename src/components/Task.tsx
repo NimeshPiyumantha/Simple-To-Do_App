d8e53d403666d39ef28f6ad3b7a4b12ca59e6efb
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-26 20:50:52 +0530

    Update Content

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,4 +1,5 @@
 import { ITask } from "@/types/tasks";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
 import React from "react";
 
 interface TaskProps {
@@ -8,8 +9,11 @@ interface TaskProps {
 const Task: React.FC<TaskProps> = ({ task }) => {
   return (
     <tr key={task.id}>
-      <td>{task.text}</td>
-      <td>blue</td>
+      <td className="w-full">{task.text}</td>
+      <td className="flex gap-5">
+        <FiEdit className="text-yellow-500" size={25} />
+        <FiTrash2 className="text-red-500" size={25} />
+      </td>
     </tr>
   );
 };
