b553ba54617d2c5073393b084c8aae21fd4ef1f8
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-26 17:49:50 +0530

    Update Content

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,23 +5,24 @@ interface TodoListProps {
   tasks: ITask[];
 }
 
-const TodoList: React.FC<TodoListProps> = () => {
+const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
   return (
     <div className="overflow-x-auto">
       <table className="table w-full">
         {/* head */}
         <thead>
           <tr className="bg-gray-200">
-            <th>Name</th>
-            <th>Favorite Color</th>
+            <th>Tasks</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          {/* row 1 */}
-          <tr>
-            <td>Cy Ganderton</td>
-            <td>Blue</td>
-          </tr>
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td>{task.text}</td>
+              <td>{}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
